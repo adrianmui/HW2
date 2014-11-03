747258b3e85d85d5b16ca79ca3221797da513bcc
adrianmui <adrian.mui@gmail.com>
2014-11-02 23:55:21 -0600

    ds

--- conway.cpp
+++ conway.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 Conway::Conway(int s) {
 	side = s;// Your code here!
-    Cell** universe = new Cell*[side];
+    universe = new Cell*[side];
     for(int i = 0; i < side; i++)
         universe[i] = new Cell[side];
 }
