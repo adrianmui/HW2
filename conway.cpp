cdf2ba4fc7b806a3bb8097111718bc7c3fa64b66
adrianmui <adrian.mui@gmail.com>
2014-11-02 22:16:14 -0600

    df

--- conway.cpp
+++ conway.cpp
@@ -32,11 +32,11 @@ int Conway::count_alive_neighbors(int x, int y)
 	int count = 0;
     for (int i = (x-1); i == (x+1); i++)
     {
-        for (int j = (y-1); y == (y+1); j++)
+        for (int j = (y-1); j == (y+1); j++)
         {
             if ((universe[i][j].get_alive()) == true)
             {
-                if ((i == x) && (j == y)) // excludes center
+                if ((i != x) && (j != y)) // excludes center
                 {
                     count++;
                 }
